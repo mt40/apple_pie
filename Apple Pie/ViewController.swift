21a6992299e92f7f9d1ddef7f85347120e37ee8c
Minh Thai <minhthai40@gmail.com>
2021-01-21 21:19:38 +0700

    add image and keyboard

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Thai Minh on 21/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
