d29b693cfd9d4e4701562f665f67d000a2a8ae8a
Minh Thai <minhthai40@gmail.com>
2021-01-24 10:23:50 +0700

    add references and actions

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet var treeImageView: UIImageView!
+  @IBOutlet var correctWordLabel: UILabel!
+  @IBOutlet var scoreLabel: UILabel!
+  @IBOutlet var letterButtons: [UIButton]!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func letterButtonPressed(_ sender: UIButton) {
+    sender.isEnabled = false
+  }
 }
 
